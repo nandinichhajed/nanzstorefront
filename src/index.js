8920b5c5f1aae534549dc5a9178cb12ef54ef384
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-11 17:28:14 +0530

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-import App from './App';
+// import App from './App';
+import Nandini from "./Routes"
 import {BrowserRouter} from "react-router-dom"
 
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Nandini />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
