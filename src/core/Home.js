1f4be089d2f72c1f4348d6b48b5231643bcde273
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-11 21:12:17 +0530

    getting all t-shirts name in frontend

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 
 export default function Home() {
+
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAllProducts = async() => {
+
+        try {
+          const { data } = await axios.get("/api/product")
+
+      
+          setProducts(data);
+
+        } 
+        catch (e) {
+          setError(e);
+          console.log(e);
+        }
+      };
+    
+    useEffect(() => {
+        loadAllProducts();
+    }, []);
+    
     return(
         <div>
             <h1>
                 Home Component
             </h1>
+            <div className="row">
+                {products.map( (product, index) => {
+                    return (
+                        <div key={index}>
+                            <h2>{product.name}</h2>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
-    )
+    );
 }
--- src/core/helper/coreapicalls.js
+++ src/core/helper/coreapicalls.js
@@ -1,8 +1,8 @@
 import {API} from "../../backend";
 
 export const getProducts = () =>{
-    return fetch(`${API}product`, { method: "GET" })
-    .then((Response) => {
+    return fetch(`/product`, { method: "GET" })
+    .then((response) => {
         return response.json();
     })
     .catch((err) => console.log(err));
