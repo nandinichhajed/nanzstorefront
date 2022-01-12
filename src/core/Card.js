869d53a29c46caed1ff95da6d06ed0a4aa034aa6
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-12 17:40:13 +0530

    pulling data from the backend

---
+++ src/core/Card.js
@@ -0,0 +1,48 @@
+import Recat from "react";
+import ImageHelper from "./helper/ImageHelper";
+
+
+const Card = ({
+    product,
+    addtoCart = true,
+    removeFromCart = false,
+}) => {
+
+    const cartTitle = product ? product.name : "A photo from pexels";
+    const cartDescription = product ? product.description : "Default description";
+    const cartPrice = product ? product.price : "Default";
+
+    return (
+      <div className="card text-white bg-dark border border-info ">
+        <div className="card-header lead">{cartTitle}</div>
+        <div className="card-body">
+          <ImageHelper product={product} />
+          <p className="lead bg-success font-weight-normal text-wrap">
+            {cartDescription}
+          </p>
+          <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
+          <div className="row">
+            <div className="col-12">
+              <button
+                onClick={() => {}}
+                className="btn btn-block btn-outline-success mt-2 mb-2"
+              >
+                Add to Cart
+              </button>
+            </div>
+            <div className="col-12">
+              <button
+                onClick={() => {}}
+                className="btn btn-block btn-outline-danger mt-2 mb-2"
+              >
+                Remove from cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+
+export default Card;
