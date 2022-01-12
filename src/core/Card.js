2f1cc16409d853f742410ab9dc36250c8d213359
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-12 19:05:35 +0530

    conditional rendering of buttons

--- src/core/Card.js
+++ src/core/Card.js
@@ -1,6 +1,8 @@
 import Recat from "react";
 import ImageHelper from "./helper/ImageHelper";
+import { Navigate } from "react-router-dom";
 
+const isAuthenticated = true;
 
 const Card = ({
     product,
@@ -11,6 +13,49 @@ const Card = ({
     const cartTitle = product ? product.name : "A photo from pexels";
     const cartDescription = product ? product.description : "Default description";
     const cartPrice = product ? product.price : "Default";
+
+    const addToCart = () => {
+        if (isAuthenticated) {
+          console.log("Added to cart");
+        } else {
+          console.log("Please Login!");
+        }
+      };
+    
+      const getAredirect = (Navigate) => {
+        if (Navigate) {
+          return < Navigate to="/cart" />;
+        }
+      };
+
+      const showAddToCart = (addToCart) => {
+        return (
+          addtoCart && (
+            <button
+              onClick={addToCart}
+              className="btn btn-block btn-outline-success mt-2 mb-2"
+            >
+              Add to Cart
+            </button>
+          )
+        );
+      };
+
+      const showRemoveFromCart = (removeFromCart) => {
+        return (
+          removeFromCart && (
+            <button
+              onClick={() => {
+                //TODO: handle this too
+                console.log("Product removed from cart");
+              }}
+              className="btn btn-block btn-outline-danger mt-2 mb-2"
+            >
+              Remove from cart
+            </button>
+          )
+        );
+      };
 
     return (
       <div className="card text-white bg-dark border border-info ">
@@ -23,20 +68,10 @@ const Card = ({
           <p className="btn btn-success rounded  btn-sm px-4">$ {cartPrice}</p>
           <div className="row">
             <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-success mt-2 mb-2"
-              >
-                Add to Cart
-              </button>
+              {showAddToCart(addToCart)}
             </div>
             <div className="col-12">
-              <button
-                onClick={() => {}}
-                className="btn btn-block btn-outline-danger mt-2 mb-2"
-              >
-                Remove from cart
-              </button>
+              {showRemoveFromCart(removeFromCart)}
             </div>
           </div>
         </div>
