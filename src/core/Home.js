fd7a335484c72abfbfeea284dead856fcbc7eb24
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-12 16:31:38 +0530

    passing prop in home file

--- src/core/Home.js
+++ src/core/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios"
 import Base from "./Base";
 import "../styles.css";
+import Card from "./Card";
 
 export default function Home() {
 
@@ -33,10 +34,10 @@ export default function Home() {
                 Home Component
             </h1>
             <div className="row">
-                {products.map( (product, index) => {
+                {products.map((product, index) => {
                     return (
-                        <div key={index}>
-                            <h2>{product.name}</h2>
+                        <div key={index} className="col-4 mb-4">
+                            <Card product={product} />
                         </div>
                     );
                 })}
