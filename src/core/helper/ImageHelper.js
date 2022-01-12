fdd16e14fe60c373f8e18669ca72a5420e3a62fa
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-12 14:12:51 +0530

    image helper function

---
+++ src/core/helper/ImageHelper.js
@@ -0,0 +1,18 @@
+import Recat from "react";
+
+const ImageHelper = ({ product }) => {
+    const imageurl = product
+    ? product.image
+    : `https://placeimg.com/640/480/nature`
+    return (
+        <div className="rounded border border-success p-2">
+            <img 
+                src={imageurl}
+                style={{ maxHeight: "100%", maxWidth: "100%" }}
+                className="mb-3 rounded"
+                alt=""
+            />
+        </div>
+    )
+
+}
