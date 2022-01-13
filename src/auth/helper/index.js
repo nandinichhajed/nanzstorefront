2aed9d3a333c7d360700227e18e39e08436f3a7e
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-13 18:18:28 +0530

    Signout and authentication functions

--- src/auth/helper/index.js
+++ src/auth/helper/index.js
@@ -44,3 +44,44 @@ export const signin = (user) => {
       })
       .catch((err) => console.log(err));
   };  
+
+export const authenticate = (data, next) => {
+    if (typeof window !== undefined) {
+        // jwt = JSON web token
+      localStorage.setItem("jwt", JSON.stringify(data));
+      next();
+    }
+  };
+  
+export const isAuthenticated = () => {
+    if (typeof window == undefined) {
+      return false;
+    }
+    if (localStorage.getItem("jwt")) {
+      return JSON.parse(localStorage.getItem("jwt"));
+      //TODO: compare JWT with database json token
+    } else {
+      return false;
+    }
+  };
+  
+export const signout = (next) => {
+    const userId = isAuthenticated() && isAuthenticated().user.id;
+  
+    // console.log("USERID: ", userId);
+  
+    if (typeof window !== undefined) {
+      localStorage.removeItem("jwt");
+      cartEmpty(() => {});
+      //next();
+  
+      return fetch(`${API}user/logout/${userId}`, {
+        method: "GET",
+      })
+        .then((response) => {
+          console.log("Signout success");
+          next();
+        })
+        .catch((err) => console.log(err));
+    }
+  };
