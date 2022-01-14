ca318628e979b499eea7df020680c9884d5d59b6
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-14 16:58:51 +0530

    creating userdashboard

---
+++ src/user/UserDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Base from "../core/Base";
+
+const UserDashboard = () => {
+  return (
+    <Base title="User dashboard" description="Dashboard for users">
+      <h1>Welcome to user dashboard page</h1>
+    </Base>
+  );
+};
+
+export default UserDashboard;
