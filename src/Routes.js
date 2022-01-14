09a3c136a571afa15f37aea9027e00923044da88
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-14 17:08:26 +0530

    adding route for signin function

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import Signup from "./user/Signup"
 import "bootstrap/dist/css/bootstrap.min.css"
 import UserDashboard from "./user/UserDashboard"
 import PrivateRoutes from "./auth/helper/PrivateRoutes"
+import Signin from "./user/Signin"
 // import "./App.css" 
 
 const Nandini = () =>{
@@ -13,6 +14,7 @@ const Nandini = () =>{
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/signup" exact element={<Signup />} />
+        <Route path="/signin" exact element={<Signin />} />
         <PrivateRoutes path="/user/dashboard" exact element={<UserDashboard />} />
       </Routes>
     </>
