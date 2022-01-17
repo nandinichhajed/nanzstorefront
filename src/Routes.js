360fe54b9ef5c6db9b93afb34f3a771dde020c22
nandinichhajed <nandinichhajed08@gmail.com>
2022-01-17 15:30:51 +0530

    Adding private routes

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,8 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import UserDashboard from "./user/UserDashboard"
 import PrivateRoutes from "./auth/helper/PrivateRoutes"
 import Signin from "./user/Signin"
+import { isAuthenticated } from "./auth/helper"
+
 // import "./App.css" 
 
 const Nandini = () =>{
@@ -15,7 +17,7 @@ const Nandini = () =>{
         <Route path="/" exact element={<Home />} />
         <Route path="/signup" exact element={<Signup />} />
         <Route path="/signin" exact element={<Signin />} />
-        <Route path="/user/dashboard" element={<UserDashboard />} />
+        <Route path="/user/dashboard" element={isAuthenticated ? <UserDashboard /> : <Signin />} />
       </Routes>
     </>
   );
